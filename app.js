950c2578776205aa8956dad952f4a55fbca4b241
Perttu <perttu@gmail.com>
2016-02-24 23:28:30 +0200

    Rewrote the search! Now utilizes an API

--- app.js
+++ app.js
@@ -1,68 +1,25 @@
 // create our angular module and inject firebase
 angular.module('App', ['firebase', 'ngAnimate'])
 
-.controller('mainController', function($scope, $firebase) {
+.controller('mainController', function($scope, $firebase, $http) {
 
-  $scope.placeholder = "ladataan...";
-  $scope.foodData = [];
+  $scope.placeholder = "hae...";
   $scope.results = [];
   $scope.searchTerm = null;
-  $scope.previousSearchTerm = '';
   $scope.searchTimer = null;
   $scope.minSearchTermLength = 4;
   $scope.searchTimeOut = 1000 * 0.8;
   $scope.showResults = false;
 
-  $scope.fb = new Firebase("https://ilenif.firebaseio.com/");
-
-  $scope.initialFetch = function() {
-    $scope.fb.once("value", function(data) {
-
-      data.forEach(function(item) {
-        $scope.foodData.push(item.val()["item"]);
-        // var o = item.val()["item"];
-        // $scope.foodData.push(
-        //   {
-        //     "Name": o["Name"],
-        //     "Enerc": $scope.joulesToCalories(o["Enerc"]),
-        //     "Choavl": o["Choavl"],
-        //     "Prot": o["Prot"],
-        //     "Fat": o["Fat"],
-        //     "FineliId": o["FineliId"]
-        //   }
-        // );
-
-      });
-      console.log('data ready');
-      $scope.placeholder = "hae...";
-      $scope.$apply();
-    });
-  }
-
-  $scope.joulesToCalories = function(val) {
-    if(val === null || val.length < 1) return "";
-
-    var dec = parseFloat(val.replace(',', '.'));
-    return (dec / 4.184).toFixed(0);
-  }
-
-  $scope.formatDecimal = function(val) {
-    if(val === null || val.length < 1) return "";
-
-    return parseFloat(val.replace(',', '.')).toFixed(1);
-  }
-
   $scope.searchTermUpdated = function() {
+
     // search term too short, do nothing, hide results
     if($scope.searchTerm == null || $scope.searchTerm.length < $scope.minSearchTermLength) {
       $scope.results = [];
       $scope.showResults = false;
-      // $scope.previousSearchTerm = $scope.searchTerm;
+
       return;
     }
-
-    // search term hasn't changed, return
-    if($scope.searchTerm == $scope.previousSearchTerm) return;
 
     // start timer
     clearTimeout($scope.searchTimer);
@@ -70,40 +27,26 @@ angular.module('App', ['firebase', 'ngAnimate'])
     // give user a moment to finish typing.
     // only start searching when specified time has passed
     $scope.searchTimer = setTimeout(function doSearch() {
-      console.log('searchTerm: %s', $scope.searchTerm);
+      // console.log('searchTerm: %s', $scope.searchTerm);
 
-      if($scope.searchTerm.length > $scope.minSearchTermLength && $scope.previousSearchTerm != "" && $scope.searchTerm.startsWith($scope.previousSearchTerm)) {
-
-        $scope.previousSearchTerm = $scope.searchTerm;
-
-        // user keeps adding to earlier search, loop through
-        // already filtered list of results
-        for(var i = $scope.results.length; i--; i >= 0) {
-          var item = $scope.results[i];
-
-          if(item.Name.indexOf($scope.searchTerm.toUpperCase()) < 0) {
-            console.log('search not found for item %s', item.Name);
-            $scope.results.splice(i, 1);
-          }
-        }
-
-      } else {
-        // search term has changed enough, start from whole data set
         $scope.results = [];
-        $scope.previousSearchTerm = $scope.searchTerm;
 
-        $scope.foodData.forEach(function(item) {
-          if(item.Name.indexOf($scope.searchTerm.toUpperCase()) > -1) $scope.results.push(item);
+        // hello server
+        $http.get('/api/search/' + $scope.searchTerm)
+        .then(function success(response) {
+          // console.log('data: %s', response);
+          // console.dir(response.data);
+
+          $scope.results = response.data;
+        },
+        function error(response) {
+          console.log('error!');
         });
-
-      }
 
       if($scope.results.length > 0) $scope.showResults = true;
       $scope.$apply();
 
     }, $scope.searchTimeOut)
   }
-
-  $scope.initialFetch();
 
 });
--- server.js
+++ server.js
@@ -1,5 +1,41 @@
 var express = require('express');
 var app = express();
+var fb = require('firebase')
+
+var foodData = [];
+
+var ref = new Firebase("https://ilenif.firebaseio.com/");
+
+function joulesToCalories(val) {
+  if(val === null || val.length < 1) return "";
+
+  var dec = parseFloat(val.replace(',', '.'));
+  return (dec / 4.184).toFixed(0);
+}
+
+function formatDecimal(val) {
+  if(val === null || val.length < 1) return "";
+
+  return parseFloat(val.replace(',', '.')).toFixed(1);
+}
+
+ref.once("value", function(data) {
+  data.forEach(function(item) {
+    var o = item.val()["item"];
+    foodData.push(
+      {
+        "name": o["Name"],
+        "enerc": formatDecimal(joulesToCalories(o["Enerc"])),
+        "carbs": formatDecimal(o["Choavl"]),
+        "prot": formatDecimal(o["Prot"]),
+        "fat": formatDecimal(o["Fat"]),
+        "fineliid": o["FineliId"]
+      }
+    );
+  });
+
+  console.dir("data ready");
+});
 
 app.use(express.static(__dirname));
 app.use(express.static('public'));
@@ -8,6 +44,25 @@ app.get('/', function(req, res) {
     res.sendfile('index.html', {root: __dirname })
 });
 
+app.get('/api', function (req, res) {
+  res.json('API is running');
+});
+
+// GET food items by search term
+app.get('/api/search/:searchterm?', function (req, res){
+  var searchTerm = req.params.searchterm;
+  console.dir('named parameter: ' + searchTerm);
+  var results = [];
+
+  foodData.forEach(function(item) {
+    if(item.name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1) results.push(item);
+  });
+
+  res.json(results);
+
+});
+
 var port = process.env.PORT || 8080;
 var server = app.listen(port);
+
 console.log("Server listening at port %s", port);
